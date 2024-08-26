c99355c84138b2b713dfa2bc012c8b59d7b02c63
Johannes Otepka <johannes.otepka@geo.tuwien.ac.at>
2024-08-26 17:13:03 +0200

    Update to version 1.8.6: * small improvement in automatic installation of requirements * metadata updated (changed links from Lukas' to TUW-GEO repository)

--- __init__.py
+++ __init__.py
@@ -21,9 +21,10 @@ email                : lukas.winiwarter@tuwien.ac.at
 from qgis.PyQt.QtCore import *
 from qgis.PyQt.QtWidgets import *
 from qgis.PyQt.QtGui import *
-import importlib
+import importlib, site
 import sys, os, glob
 
+from qgis.core import QgsMessageLog, Qgis
 
 def name(): 
     return "qpals"
@@ -40,38 +41,69 @@ def version():
 def qgisMinimumVersion():
     return "2.99"
 
+def check_packages(packages):
+    installed_packages = []
+    missing_packages = []
+    for package in packages:
+        try:
+            importlib.import_module(package)
+            installed_packages.append(package)
+        except:
+            missing_packages.append(package)
+    return installed_packages, missing_packages
+
 
 def classFactory(iface):
     # check requirements
-    for package in ['matplotlib', 'vispy', 'scipy', 'semantic_version']:
-        try:
-            importlib.import_module(package)
-        except:
-            msg = QMessageBox()
-            msg.setText("A package that is required for qpals could not be found")
-            msg.setInformativeText("Qpals requires the package '%s'. Press 'OK' to attempt installation (might take some time)." % package)
-            msg.setWindowTitle("qpals missing libraries")
-            msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-            ret = msg.exec_()
-            if ret == QMessageBox.Ok:
-                #    import pip
-                exe = sys.executable
-                files = glob.glob(os.path.join(os.path.split(exe)[0], "python-qgis*.bat"))
-                if len(files) == 1:
-                    python_exe = files[0]
-                    import subprocess
-                    rc = subprocess.call([python_exe, '-m', 'pip', 'install', package])
-                    msg = QMessageBox()
-                    msg.setText("Package installation")
-                    msg.setWindowTitle("qpals package installation")
-                    msg.setStandardButtons(QMessageBox.Ok)
-                    if rc == 0:
-                        msg.setInformativeText("Package installation succeeded")
-                    else:
-                        msg.setInformativeText("Package installation failed. Please try manual installation. Not all features of qpals will be available.")
+    QgsMessageLog.logMessage('check requirements', 'qpals', level=Qgis.Info)
+
+    packages = ['matplotlib', 'vispy', 'scipy', 'semantic_version']
+    _, missing_packages = check_packages(packages)
+
+    QgsMessageLog.logMessage(f'missing packages={missing_packages}', 'qpals', level=Qgis.Info)
+
+    if missing_packages:
+        msg = QMessageBox()
+        msg.setText("Packages that are required for qpals could not be found")
+        msg.setInformativeText(f"Qpals requires packages {', '.join(missing_packages)}. Press 'OK' to attempt installation (might take some time).")
+        msg.setWindowTitle("qpals missing libraries")
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        ret = msg.exec_()
+        if ret == QMessageBox.Ok:
+            #    import pip
+            exe = sys.executable
+            files = glob.glob(os.path.join(os.path.split(exe)[0], "python-qgis*.bat"))
+            installation_ok = False
+            if len(files) == 1:
+                python_exe = files[0]
+                import subprocess
+                rc = subprocess.call([python_exe, '-m', 'pip', 'install'] + missing_packages)
+                msg = QMessageBox()
+                msg.setText("Package installation")
+                msg.setWindowTitle("qpals package installation")
+                msg.setStandardButtons(QMessageBox.Ok)
+                if rc == 0:
+                    _, missing_packages = check_packages(missing_packages)
+                    QgsMessageLog.logMessage(f'missing packages after installation={missing_packages}', 'qpals', level=Qgis.Warning)
+                    if missing_packages:
+                        site.main()     # importing doesn't work if user site directory didn't exist before. So try re-initializing
+                        _, missing_packages = check_packages(missing_packages)
+                        QgsMessageLog.logMessage(f'missing packages after site.main()={missing_packages}', 'qpals',
+                                                 level=Qgis.Warning)
+                        if missing_packages:
+                            msg.setInformativeText("Package installation succeeded but importing failed. Try to restart QGIS")
+
+                    if not missing_packages:
+                        installation_ok = True
                 else:
-                        msg.setInformativeText("Package installation failed (Unable to locate qgis python startup script). Please try manual installation. Not all features of qpals will be available.")
+                    msg.setInformativeText("Package installation failed. Please try manual installation. Not all features of qpals will be available.")
+            else:
+                    msg.setInformativeText("Package installation failed (Unable to locate qgis python startup script). Please try manual installation. Not all features of qpals will be available.")
+            if not installation_ok:
                 ret = msg.exec_()
+            else:
+                QgsMessageLog.logMessage(f'packages installed successful', 'qpals',
+                                         level=Qgis.Info)
 
     # load qpals class from file qpals
     from .pyqpals import qpals
